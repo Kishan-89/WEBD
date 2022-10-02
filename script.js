992e7edcc2c704ae46f3d2ffb8501f84a64a27ed
Kishan Singh <kishanrsingh123@gmail.com>
2022-10-02 12:22:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function bulbToggle(sw){
 
     document.getElementById("bulbImage").src=pic;
 }
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
